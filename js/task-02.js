94d3b51c70d9e1a9cd47fe7cd6467185a59ac608
orellesha9 <orellesha9@gmail.com>
2023-10-20 23:31:11 +0300

    complete_task.2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const liElements = document.querySelector("ul");
+
+
+ingredients.forEach(function (value) {
+  const liElement = document.createElement("li")
+  liElement.classList.add("item");
+  liElement.textContent = value;
+  liElements.append(liElement);
+});
+document.body.append(liElements); 
