9a7f60aba78e9794d9cb03075db66103e9afe80f
orellesha9 <orellesha9@gmail.com>
2023-10-22 22:16:16 +0300

    finish_8_task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const login = form.elements.email.value;
+    const password = form.elements.password.value;
+    const userObject = { Login: login, Password: password };
+
+    if (login === "" || password === "") {
+        alert("Всі поля мають бути заповнені");
+        return;
+    }
+
+    console.log(userObject);
+    form.reset();
+}
