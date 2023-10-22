f062118b8fe5be7d808e8eaf4213f942456936d8
orellesha9 <orellesha9@gmail.com>
2023-10-22 22:59:44 +0300

    finish_9_task

--- js/task-01.js
+++ js/task-01.js
@@ -4,10 +4,10 @@ console.log(`Number of categories: ${categoryItems.length}`);
 categoryItems.forEach(categoryItem => {
     const categoryTitle = categoryItem.querySelector("h2");
 
-  // Знаходимо всі підкатегорії (всі li вкладені в ul)
+
   const subCategories = categoryItem.querySelectorAll("ul li");
 
-  // Виводимо інформацію про категорію
+ 
   console.log(`Category: ${categoryTitle.textContent}`);
   console.log(`Elements: ${subCategories.length}`);
 });
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const changeColor = document.querySelector(".widget");
+const bgrColor = changeColor.querySelector(".color");
+
+
+changeColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  bgrColor.innerHTML = randomColor;
+  document.body.style.backgroundColor = randomColor;
+});
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
